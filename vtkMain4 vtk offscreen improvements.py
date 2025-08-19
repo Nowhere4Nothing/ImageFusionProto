5627fc241a1d46d9345b38f612e8919f42196ac8
Matt <mattbroganmusic@gmail.com>
2025-08-19 23:20:15 +1000

    Fixed transform matrix initialisation
    - Initialise matrix with starting values

--- vtkMain4 vtk offscreen improvements.py
+++ vtkMain4 vtk offscreen improvements.py
@@ -198,19 +198,38 @@ class TransformMatrixDialog(QtWidgets.QDialog):
         super().__init__(parent)
         self.setWindowTitle("Transformation Matrix (4x4)")
         self.resize(400, 200)
+
         layout = QtWidgets.QVBoxLayout(self)
         self.table = QtWidgets.QTableWidget(4, 4)
         self.table.horizontalHeader().hide()
         self.table.verticalHeader().hide()
+        self.table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         layout.addWidget(self.table)
+
+        # Initialize with identity matrix
+        self._init_identity_matrix()
+
+    def _init_identity_matrix(self):
+        identity = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ]
+        for i in range(4):
+            for j in range(4):
+                item = QtWidgets.QTableWidgetItem(f"{identity[i][j]:.2f}")
+                item.setTextAlignment(QtCore.Qt.AlignCenter)
+                self.table.setItem(i, j, item)
 
     def set_matrix(self, vtk_transform: vtk.vtkTransform):
         mat = vtk_transform.GetMatrix()
         for i in range(4):
             for j in range(4):
-                item = QtWidgets.QTableWidgetItem(f"{mat.GetElement(i,j): .3f}")
+                item = QtWidgets.QTableWidgetItem(f"{mat.GetElement(i,j):.2f}")
                 item.setTextAlignment(QtCore.Qt.AlignCenter)
                 self.table.setItem(i, j, item)
+
 
 # ------------------------------ Qt Display ------------------------------
 
@@ -344,7 +363,7 @@ class FusionUI(QtWidgets.QWidget):
 # ------------------------------ Controller ------------------------------
 
 class Controller(QtCore.QObject):
-    DEBOUNCE_MS = 10
+    DEBOUNCE_MS = 0
     def __init__(self, ui: FusionUI, engine: VTKEngine):
         super().__init__()
         self.ui = ui
